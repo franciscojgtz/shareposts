7e7c35972eba5b99b81ae3a35dbeb926f882f6c0
Francisco Gutierrez <francisco.j.gtz@gmail.com>
2018-10-10 17:44:56 -0700

    feat: Inital commit

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+    //DB Config
+    require_once 'db_config';
+
+    //App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root 
+    define('URLROOT', 'http://localhost/portfolio/shareposts');
+    // Site Name
+    define('SITENAME', 'SharePosts');
