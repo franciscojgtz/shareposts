5be1ef489c6a4988c0820dcb6002f8a1e9c01950
Francisco Gutierrez <francisco.j.gtz@gmail.com>
2018-10-14 19:30:47 -0700

    feat: Add Posts Access Control

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,5 +1,11 @@
 <?php
     class Posts extends Controller {
+        public function __construct() {
+            if(!isLoggedin()) {
+                redirect('users/login');
+            }
+        }
+
         public function index() {
             $data = [];
 
